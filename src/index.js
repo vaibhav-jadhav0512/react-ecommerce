02ecd1b188da290e64b9a4355036776b58069660
Vaibhav Jadhav <vaibhav.jadhav@idrive.com>
2022-12-20 12:03:04 +0530

    redux store implemented

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import { rootReducer } from "./reducers";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = createStore(rootReducer, composeWithDevTools());
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
